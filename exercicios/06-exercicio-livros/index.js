ccbc8180ddd32743e78c1302a953bbc1d55af4eb
Matheus Bruno <matheussbsiilva@gmail.com>
2025-10-20 21:11:20 -0300

    adicionando 06 exercicios livros

---
+++ exercicios/06-exercicio-livros/index.js
@@ -0,0 +1,82 @@
+// Crio aplicação express
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// Conectar MongoDB
+const mongoose = require("mongoose");
+require('dotenv').config()
+
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+const DB_NAME = process.env.DB_NAME
+
+
+const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=IESB`
+
+mongoose.connect(url)
+    .then(() => {
+        console.log("Conectado ao banco MongoDB!!!!")
+    })
+    .catch(erro => {
+        console.log("erro ao conectar no MongoDB: ", erro)
+    })
+
+//Model (modelo) - interface com o meu banco de dados
+// Cada model representa uma collection(Tabela)
+const livroModel = mongoose.model('Livros', new mongoose.Schema(
+    {
+        titulo: String,
+        autor: String,
+        editora: String,
+        ano: Number,
+        preco: Number,
+        
+    }
+))
+
+// CRUD
+// Create
+app.post('/livros', async (req, res, next) => {
+    const livro = req.body
+    if(!livro.titulo || !livro.autor || !livro.editora || !livro.ano || !livro.preco) {
+        return res.status(400).json({erro: "Campos titulo, autor, editora, ano e preço são obrigatórios!!!"})
+    }
+    const livroCriado = await livroModel.create(livro)
+    res.status(201).json(livroCriado)
+})
+
+// Leitura
+app.get('/livros', async (req, res, next) => {
+    const livros = await PessoaModel.find()
+    res.json(livros)
+  })
+
+app.put('/livros/:id', async (req, res, next) => {
+    const id = req.params.id
+    const livro = req.body
+    if (!livro.titulo || !livro.autor || !livro.editora || !livro.ano || !livro.preco) {
+      return res.status(400).json({ erro: "Campos titulo, autor, editora, ano e preço são obrigatórios!!!" })
+    }
+    const livroAtualizado = await livroModel.findByIdAndUpdate(id, livro, { new: true })
+    if (!livroAtualizado) {
+      return res.status(404).json({ erro: "Livro não encontrado!" })
+    }
+    res.json(livroAtualizado)
+  })
+//  Exclusão
+app.delete('/livros/:id', async (req, res, next) => {
+    const id = req.params.id
+    await livroModel.findByIdAndDelete(id)
+    res.status(204)
+})
+
+
+
+
+app.listen(3000, () => {
+  console.log("aplicação rodando em http://localhost:3000");
+});
+
--- praticas/06-crud-pessoas/index.js
+++ praticas/06-crud-pessoas/index.js
@@ -45,10 +45,29 @@ app.post('/pessoas', async (req, res, next) => {
     res.status(201).json(pessoaCriada)
 })
 
-//HEAD
-app.get('/pessoas', async (req,res, next) =>{
-    const pessoas = await pessoaModel.find()
+// Leitura
+app.get('/pessoas', async (req, res, next) => {
+    const pessoas = await PessoaModel.find()
     res.json(pessoas)
+  })
+
+app.put('/pessoas/:id', async (req, res, next) => {
+    const id = req.params.id
+    const pessoa = req.body
+    if (!pessoa.nome || !pessoa.idade) {
+      return res.status(400).json({ erro: "Campos nome e idade são obrigatórios!" })
+    }
+    const pessoaAtualizada = await PessoaModel.findByIdAndUpdate(id, pessoa, { new: true })
+    if (!pessoaAtualizada) {
+      return res.status(404).json({ erro: "Pessoa não encontrada!" })
+    }
+    res.json(pessoaAtualizada)
+  })
+//  Exclusão
+app.delete('/pessoas/:id', async (req, res, next) => {
+    const id = req.params.id
+    await pessoaModel.findByIdAndDelete(id)
+    res.status(204)
 })
 
 
